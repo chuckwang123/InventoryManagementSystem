4bc45759b5737af2f69d9d18bf79c296d64b10a6
chuckwang123 <wangjohn419@gmail.com>
2016-04-17 20:48:06 -0500

    Null

---
+++ InventoryManagementSystem/InventoryManagementSystem/scripts/App/Stock-Controller.js
@@ -0,0 +1,17 @@
+﻿angular.module('StockApp', [])
+    .controller('StockController', function ($scope, $http) {
+        $scope.title = "Stock Info";
+        $scope.working = false;
+        $scope.StockCollection = function () {
+            $http.get("/api/Stock").success(function (data, status, headers, config) {
+                $scope.Stocks = [];
+                angular.forEach(data, function (value, key) {
+                    $scope.Stocks.push(value);
+                });
+                $scope.working = false;
+            }).error(function (data, status, headers, config) {
+                $scope.title = "Oops... something went wrong";
+                $scope.working = false;
+            });
+        };
+    });
