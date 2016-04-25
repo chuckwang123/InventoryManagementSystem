544a7389515510f70cd5007e6146a0ab3492ccbe
chuckwang123 <wangjohn419@gmail.com>
2016-04-25 00:11:12 -0500

    Update Nuget Add Html and JQuery Try use AngularJS

---
+++ InventoryManagementSystem/InventoryManagementSystem/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace InventoryManagementSystem.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
